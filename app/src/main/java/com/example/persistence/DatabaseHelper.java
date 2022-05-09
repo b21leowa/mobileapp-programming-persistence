1de62b0fb33d079240a5e0f2adc5073b42c92b8b
b21leowa <b21leowa@student.his.se>
2022-05-09 12:48:04 +0200

    Structured the databaseTable and added DatabaseHelper

--- app/src/main/java/com/example/persistence/DatabaseHelper.java
+++ app/src/main/java/com/example/persistence/DatabaseHelper.java
@@ -1,2 +1,25 @@
-package com.example.persistence;public class DatabaseHelper {
+package com.example.persistence;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1; // If this is incremented onUpgrade() will be executed
+    private static final String DATABASE_NAME = "Cars.db"; // The file name of our database
+
+    DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
--- app/src/main/java/com/example/persistence/DatabaseTables.java
+++ app/src/main/java/com/example/persistence/DatabaseTables.java
@@ -1,2 +1,19 @@
-package com.example.persistence;public class DatabaseTables {
+package com.example.persistence;
+
+public class DatabaseTables {
+    static class Cars {
+        static final String TABLE_NAME = "cars";
+        static final String COLUMN_NAME_ID = "id";
+        static final String COLUMN_NAME_BRAND = "brand";
+        static final String COLUMN_NAME_MODEL = "model";
+        static final String COLUMN_NAME_TOPSPEED = "topSpeed";
+    }
+
+    static final String SQL_CREATE_TABLE_CARS =
+            // "CREATE TABLE cars (id INTEGER PRIMARY KEY, brand TEXT, model TEXT, topSpeed INT)"
+                "CREATE TABLE " + Cars.TABLE_NAME + " (" +
+                        Cars.COLUMN_NAME_ID + "INTEGER PRIMARY KEY," +
+                        Cars.COLUMN_NAME_BRAND + " TEXT," +
+                        Cars.COLUMN_NAME_MODEL + " TEXT," +
+                        Cars.COLUMN_NAME_TOPSPEED + " INT)";
 }
